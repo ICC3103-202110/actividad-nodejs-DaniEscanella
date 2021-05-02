e0ae011b0fb2cbad208921cd845db34f5c318942
Daniela Escanella <dani@MacBook-Pro-de-Daniela-94.local>
2021-05-02 19:56:25 -0400

    Aprendiendo Java

---
+++ actividad-js/act_js.js
@@ -0,0 +1,34 @@
+var nombre = "Daniela";
+var edad = 20;
+
+var edad; //Declaro la variable 
+edad =20; //Inicializar variable
+
+console.log(nombre , "tiene",edad, "años");
+
+// VAriable complejas
+
+//Variables tipo objeto
+
+var persona = {
+    nombre: "Daniela",
+    apellido: "Esca",
+    edad: 20
+}
+
+console.log(persona);
+console.log();
+// Array
+
+var amigos =["Santiago","Pedro","Juan", "Diego"];
+var edades =[21,19,20,12];
+console.log(amigos);
+
+var amigos =[
+    {nombre: 'Santiago', edad: 21},
+    {nombre: 'Pedro', edad: 19},
+    {nombre: 'Juan', edad: 20},
+    {nombre: 'Diego', edad: 12}
+];
+
+console.log(amigos);
