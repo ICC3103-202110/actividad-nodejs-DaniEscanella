a1b8b5f032afea66ec6d80b014404e1313dac13c
Daniela Escanella <dani@MacBook-Pro-de-Daniela-94.local>
2021-05-02 21:12:03 -0400

    Actividad lista

--- actividad-js/act_js.js
+++ actividad-js/act_js.js
@@ -18,6 +18,6 @@ function fio(num){
 readline.question(`Choose a number: `, number => {
 
     total=fio(number);
-    console.log(total)
+    console.log(`F(${number}) = ${total}`)
     readline.close()
 });
